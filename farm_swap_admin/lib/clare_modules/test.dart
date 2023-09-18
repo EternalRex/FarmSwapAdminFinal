a89f5b8af5b30dff48e270df479e5e601fca6de9
KerKee <clarenicor22@gmail.com>
2023-09-18 14:28:59 +0800

    clare

---
+++ farm_swap_admin/lib/clare_modules/test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ClareTest extends StatelessWidget {
+  const ClareTest({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
